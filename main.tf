733d6bc08bbfb71b58a7a287d1ab9e0035093f2b
Ubuntu <ubuntu@ip-172-31-13-215.eu-west-3.compute.internal>
2021-01-25 15:05:11 +0000

    edit main#3

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "web" {
 }
 
 
-resource "aws_security_group" "security_group_jenkins_jeremy" {
+resource "aws_security_group" "securite_groupee" {
   name = "instance_terraform_jeremy"
   description = "security group jenkins jeremy"
 
